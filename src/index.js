e3f0f673edbcfdf992650a193be6f41c06dc840f
valentyn19939 <valik.hai1993@gmail.com>
2025-01-28 12:28:45 +0100

    add task solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3005;
+
+app.get('/', (req, res) => {
+  res.send('Hello bro, server is listening!');
+});
+
+app.listen(port, () => {
+  // console.log(`Server started on http://localhost:${port}`);
+});
