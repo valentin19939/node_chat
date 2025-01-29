79ec6f5b22f8dd3087b6513d2f5ff25bf9518a50
valentyn19939 <valik.hai1993@gmail.com>
2025-01-29 11:14:33 +0100

    add task solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,57 @@
+/* eslint-disable no-console */
 'use strict';
-
 import express from 'express';
+import 'dotenv/config';
 import cors from 'cors';
+import { messageRouter } from './routes/message.route.js';
+import { roomRouter } from './routes/room.route.js';
+import { WebSocket, WebSocketServer } from 'ws';
+import { message } from './controllers/message.js';
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 3005;
+app.use(roomRouter);
+app.use(messageRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello bro, server is listening!');
+  res.send('Hello');
 });
 
-app.listen(port, () => {
-  // console.log(`Server started on http://localhost:${port}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (client) => {
+  console.log('Client connected');
+
+  client.on('message', async (data) => {
+    const receivedData = JSON.parse(data.toString());
+
+    console.log(receivedData);
+
+    const newMessage = receivedData.message;
+    const createdMessage = await message.createMessage(
+      newMessage.author,
+      newMessage.text,
+      newMessage.roomId,
+    );
+
+    console.log(createdMessage);
+
+    wss.clients.forEach((cl) => {
+      if (cl.readyState === WebSocket.OPEN) {
+        cl.send(JSON.stringify(createdMessage));
+      }
+    });
+  });
+
+  client.on('close', () => {
+    console.log('Client disconnected');
+  });
 });
